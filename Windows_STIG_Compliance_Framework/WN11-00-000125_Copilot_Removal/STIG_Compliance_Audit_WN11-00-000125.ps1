813ed6fcc7238065197528198e509a7842a6f039
Alex <127744440+4Al3xG4mesYY@users.noreply.github.com>
2026-09-21 15:53:13 -0400

    Add files via upload

---
+++ Windows_STIG_Compliance_Framework/WN11-00-000125_Copilot_Removal/STIG_Compliance_Audit_WN11-00-000125.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host ("========= Copilot Tool Removal ==========") -ForegroundColor Cyan
+$CopilotPackage = Get-AppxPackage -AllUsers | Where-Object { $_.Name -like "*Copilot*"}
+
+if($CopilotPackage)
+{
+    Write-Host ("Detected Copilot Package...") -ForegroundColor Red
+    Write-Host ("Proceed Copilot Package Removal...") -ForegroundColor Yellow
+    Get-AppxPackage -AllUsers *CoPilot* | Remove-AppxPackage -AllUsers
+    
+    $Verify = Get-AppxPackage -AllUsers | Where-Object { $_.Name -like "*Copilot*"}
+
+    if(-not $Verify)
+    {
+        Write-Host "Copilot Successfully Removed!" -ForegroundColor Green
+    }
+
+    else
+    {
+        Write-Host "Copilot still detected." -ForegroundColor Red
+    }
+}
+else{
+    Write-Host ("No Copilot package detected") -ForegroundColor Green
+}
+Write-Host ("Copilot Removal Completed!") -ForegroundColor Green
